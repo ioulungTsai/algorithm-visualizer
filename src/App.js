04c77b721a445b2496f4d31c0395a29fe9eff46d
unknown <brandonya125@gmail.com>
2022-06-02 08:50:36 -0700

    create AlgorithmVisualizer

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import AlgorithmVisualizer from './components/AlgorithmVisualizer'
+import './App.css';
+
+function App() {
+  return (
+    <AlgorithmVisualizer />
+  );
+}
+
+export default App;
---
+++ src/components/AlgorithmVisualizer.jsx
@@ -0,0 +1,7 @@
+export default function AlgorithmVisualizer() {
+    return (
+        <div className="AlgorithmVisualizer">
+            <h1>Algorithm Visualizer</h1>
+        </div>
+    )
+}
